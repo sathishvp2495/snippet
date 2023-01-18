ddd77a3d1b9fa321a8862b0f6393f6364a40c760
Sathish V <sathishvp2495@gmail.com>
2023-01-18 10:49:19 +0530

    Initial Commit

---
+++ snippet/src/main/java/com/example/snippet1/Snippet1Application.java
@@ -0,0 +1,13 @@
+package com.example.snippet1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Snippet1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Snippet1Application.class, args);
+	}
+
+}
---
+++ snippet/src/test/java/com/example/snippet1/Snippet1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.snippet1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Snippet1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
